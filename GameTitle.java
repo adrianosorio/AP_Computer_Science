2cbb56add82920b44ccbedba9d7ca53bb9ce5c67
OITAO <adrianosorio101@gmail.com>
2015-09-08 21:52:51 -0700

    This is my first assignment for the Non-routine GameTitle Project

---
+++ GameTitle.java
@@ -0,0 +1,46 @@
+public class GameTitle {
+  public static void main(String[] args) {
+    
+    //These are the characters that will  be used to draw my Game character 
+    String layerFour = "      [.........      .............      ..]";
+    String layerFive = "      [.....             .......            ";
+    String layerEight = "      [.......................................]";
+    String layerTwelve = "      [.....   .........      ........   .....]"; 
+    String layerThirteen = "      [...       .......      ......       ...]";
+    String layerFourteen = "      [..          .....      ....          ..]";
+    
+    System.out.println("                __________________");
+     System.out.println("               [..................]");//layer 1
+     System.out.println("            ___[..................]___");
+     System.out.println("           [..........................]");//layer 2
+     System.out.println("         __[..........................]__");
+     System.out.println("        [................................]");//layer 3
+     System.out.println("       _[................................]_");
+     System.out.println(layerFour);// Layer 4
+     System.out.println(layerFour);
+     System.out.println(layerFive);// layer 5
+     System.out.println(layerFive);
+     System.out.println("      [.....     #####   .......     ###### ");// layer 6
+     System.out.println("      [.....     #####   .......     ###### __");
+     System.out.println("      [.......   #####.............  ###### ..]");// layer 7
+     System.out.println("      [.......        .............       ....]");
+     System.out.println(layerEight);// layer 8
+     System.out.println(layerEight);
+     System.out.println(layerEight);// layer 9
+     System.out.println(layerEight);
+     System.out.println(layerEight);// layer 10
+     System.out.println(layerEight);
+     System.out.println(layerEight);// layer 11
+     System.out.println(layerTwelve);
+     System.out.println(layerTwelve);// Layer 12
+     System.out.println(layerThirteen);
+     System.out.println(layerThirteen);// Layer 13
+     System.out.println(layerFourteen);
+     System.out.println(layerFourteen);// Layer 14
+      System.out.println("      ---          -----      ----          ---");
+   
+      
+      
+  }
+   
+}
