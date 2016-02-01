7abf32549faa53cb03adcc01b18e06f281ee4b7a
Adrian Osorio <adrianosorio101@gmail.com>
2016-02-01 07:22:17 -0800

    Comments
    Added 3 comments for three methods

--- Notes/Animal/SuperClass.java
+++ Notes/Animal/SuperClass.java
@@ -1,15 +1,15 @@
 public class SuperClass{
     
     public SuperClass() {
-        System.out.println("A new animal has been created");
+        System.out.println("A new animal has been created");// constructor method takes no input
     }
     
     public void sleep() {
-        System.out.println("An animal Sleeps");
+        System.out.println("An animal Sleeps");// returns nothing and prints sleep
     }
     
     public void eat() {
-        System.out.println("An animal eats num num num");
+        System.out.println("An animal eats num num num");//returns nothing but prints eat
     }
     
 }
